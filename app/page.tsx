6a0175b738a9b748e933e706ea4715564bb3987c
{uneneegles} <une@neegles.com>
2026-08-31 21:18:08 +0100

    Basic HomePage Structure

--- app/page.tsx
+++ app/page.tsx
@@ -2,68 +2,83 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert h-5 w-[100px]"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the{" "}
-            <code className="rounded bg-black/[.06] px-1.5 py-0.5 font-mono text-[0.9em] dark:bg-white/[.08]">
-              page.tsx
-            </code>{" "}
-            file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
+    <div className="flex w-screen flex-col">
+      <div className="flex h-[10vh] w-screen flex-row bg-[#E2E3E1]">
+        <div className="flex flex-1 flex-row"></div>
+        <div className="flex flex-1 flex-row items-center justify-center gap-5">
+          <button className="flex-1 text-xl hover:cursor-pointer hover:text-[#7c4b8b]">
+            Home
+          </button>
+          <button className="flex-1 text-xl hover:cursor-pointer hover:text-[#7c4b8b]">
+            Shop Bundles
+          </button>
+          <button className="flex-1 text-xl hover:cursor-pointer hover:text-[#7c4b8b]">
+            My Orders
+          </button>
+          <button className="flex-1 text-xl hover:cursor-pointer hover:text-[#7c4b8b]">
+            Admin Portal
+          </button>
+        </div>
+        <div className="flex flex-1 flex-row">{/* Cart Image */}</div>
+      </div>
+      <div className="flex h-[90vh] flex-col items-center justify-center">
+        <p className="text-3xl font-bold">Your New Hair Obsession</p>
+        <p className="text-2xl text-[#7c4b8b]">Untamed Elegance</p>
+        <div className="w-3/4 text-center">
+          <p className="text-wrap">
+            Discover the industry's most sought-after raw and virgin hair
+            extensions, meticulously sourced and crafted for the discerning
+            professional.
           </p>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert h-[14px] w-4"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={14}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+        <div className="mt-5 flex flex-row gap-5">
+          <button className="rounded bg-[#7c4b8b] px-4 py-2 text-white hover:bg-transparent hover:text-black">
+            Explore Bundles
+          </button>
+          <button className="rounded bg-[#7c4b8b] px-4 py-2 text-white hover:bg-transparent hover:text-black">
+            The KUT Standard
+          </button>
         </div>
-      </main>
+      </div>
+      <div className="flex h-[50vh] flex-row gap-5 p-10">
+        <div className="flex flex-1 flex-col">
+          <p>Body Wave</p>
+        </div>
+        <div className="flex flex-1 flex-col">
+          <div className="flex-1">Deep Wave</div>
+          <div className="flex-1">French Curls</div>
+        </div>
+      </div>
+      <div className="flex h-[80vh] w-screen bg-[#fffac6]">
+        <div className="flex flex-1 flex-row gap-5 p-10">
+          <div className="flex flex-1 flex-col">
+            <p className="text-2xl font-bold">Why KUT</p>
+            <h1 className="text-6xl font-semibold">
+              Elevating the Art of Hair.
+            </h1>
+            <p className="text-wrap">
+              We bypass middlemen to source directly from temples, ensuring
+              every bundle meets our obsessive standards for cuticle alignment,
+              density, and longevity.
+            </p>
+            <div className="flex flex-row">
+              <div className="flex flex-1"></div>
+              <div className="flex flex-1"></div>
+            </div>
+            <div className=""></div>
+          </div>
+          <div className="flex flex-1">Images go here</div>
+        </div>
+      </div>
+
+      <div className="flex h-[20vh] flex-row">
+        <div className="flex flex-3">
+          <div className="flex flex-col"></div>
+          <div className="flex flex-col"></div>
+          <div className="flex flex-col"></div>
+        </div>
+        <div className="flex flex-1"></div>
+      </div>
     </div>
   );
 }
